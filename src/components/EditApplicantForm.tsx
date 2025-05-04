55e50a085274af8f9a3424bb545e357e26275095
ayan chatterjee <ayanc679@gmail.com>
2025-05-04 16:21:11 +0530

    Update EditApplicantForm.tsx

--- src/components/EditApplicantForm.tsx
+++ src/components/EditApplicantForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 export default function EditApplicantForm({ id }: { id: string }) {
   const router = useRouter()
